09af8f704c975f395e54ba627241e1afa4ba8fad
Dominik <domkwi18@gmail.com>
2022-12-01 21:01:01 +0100

    Init

---
+++ CreditCalculator/CreditCalculator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CreditCalculator
+//
+//  Created by Dominik Kwiecien on 01/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CreditCalculator/CreditCalculator/CreditCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCalculatorApp.swift
+//  CreditCalculator
+//
+//  Created by Dominik Kwiecien on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CreditCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
