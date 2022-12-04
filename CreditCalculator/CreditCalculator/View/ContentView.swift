4f57a46f0439cf2640558370158be7dcc904dfb1
Dominik <domkwi18@gmail.com>
2022-12-04 18:39:02 +0100

    Slider values changes

--- CreditCalculator/CreditCalculator/View/ContentView.swift
+++ CreditCalculator/CreditCalculator/View/ContentView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount: Float = 0.0
     @State private var installment: Float = 0.0
-    
+        
     var body: some View {
         NavigationView {
             VStack {
                 Slider(
-                    value: $installment,
-                    in:1000...100000)
+                    value: $amount,
+                    in: 1000...30000,
+                    step: 100.0,
+                minimumValueLabel: Text("1000"),
+                maximumValueLabel: Text("30000"),
+                    label: {
+                        Text("Ammount")
+                    }
+                )
+                .accentColor(.green)
                 
-                Text("loan amount \(installment)")
+                Text(
+                    String(format: "loan ammount " + "%.2f", amount)
+                )
+                
+                Slider(
+                    value: $installment,
+                    in: 6...32,
+                    step: 1.00,
+                    minimumValueLabel: Text("6"),
+                    maximumValueLabel: Text("32"),
+                    label: {
+                        Text("instalment")
+                    }
+                )
+                
+                Text(
+                    String(format:"number of installments " + "%.0f", installment)
+                )
                     .padding()
                 
                 Button("Calculate" ,action: {
